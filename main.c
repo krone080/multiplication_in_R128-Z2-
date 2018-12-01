0080633040cc99b2e81edf1b1a559b967664e2f6
Ivan Pavlov <stone08@mail.ru>
2018-12-02 01:51:38 +0300

    commit 2

--- main.c
+++ main.c
@@ -9,28 +9,28 @@
 
 //typedef unsigned long long int ull;
 
-typedef  long long int ll;
 
 void print128_num(__m128i var)
  {
  int64_t *v64val = (int64_t*) &var;
- printf("%.16llx %.16llx\n", v64val[1], v64val[0]);
+ printf("%.16lli %.16lli\n", v64val[1], v64val[0]);
  }
 
 unsigned int deg(const __m128i a)
  {
  unsigned int j=0;
- long long int* b=&a;
- while(b[1])
+ __m128i b=a;
+ unsigned long long int* b1=&b;
+ while(b1[1])
   {
-  b[1]>>1;
+  b1[1]>>=1;
   ++j;
   }
  if(j>0)
   return 64+j;
- while(b[0])
+ while(b1[0])
   {
-  b[0]>>1;
+  b1[0]>>=1;
   ++j;
   }
  return j;
@@ -38,17 +38,30 @@ unsigned int deg(const __m128i a)
 
 __m128i first_mul_mod(__m128i a, __m128i b)
  {
- ll *i=&a,*j=&b;
- __m128i c={0,0},null128={0,0},gz={0,0x87};
- while(!(j[0]==0&&j[1]==0))
+ unsigned long long *a1=&a,*b1=&b,olda11;
+ long long c1[2]={0,0};
+ unsigned k=0;
+ while(!(b1[0]==0&&b1[1]==0))
   {
-  if(j[0]&1)
-   c^=a;
-  a=((a<<1)+_mm_set_epi64x(i[0]&0x8000000000000000,0))^(i[1]&0x8000000000000000?gz:null128);
-  b==(b>>1)+_mm_set_epi64x(0,j[1]&1<<63);
+  if(b1[0]&1)
+   {
+   c1[0]^=a[0];
+   c1[1]^=a[1];
+   }
+//a=((a<<1)+_mm_set_epi64x(a1[0]&0x8000000000000000,0))^(a1[1]&0x8000000000000000?{0x87,0}:{0,0});
+  olda11=a1[1];
+  a1[1]=((a1[1]<<1)+(a1[0]&0x8000000000000000));
+  a1[0]=(a1[0]<<1)^(olda11 & 0x8000000000000000 ? 0x87 : 0);
+
+//b=(b>>1)+_mm_set_epi64x(0,(b1[1]&1)<<63);
+  b[0]=(b[0]>>1)+b1[1]&1;
+  b[1]>>=1;
+
+  ++k;
   }
+ __m128i c={c1[0],c1[1]};
  return c;
- }
+ }//9223372036854775808
 
 __m128i second_mul_mod(const __m128i a, const __m128i b)
  {
@@ -72,26 +85,26 @@ __m128i second_mul_mod(const __m128i a, const __m128i b)
 //qz оказалось равно g(x), на всякий случай pz=x^14+x^4+x^2+1,
 //deg(qz)=128, что не совсем подходит под тип __m128;
 //поэтому qz делим на коэффициент qz_up=1 при x^128 и остальную часть qz_low
- __m128i qz_up={0,1}, qz_low={0,0x87};
+ __m128i qz_up={1,0}, qz_low={0x87,0};
 
  //long long int buf1[2]=_mm_clmulepi64_si128(c,qz_up,1);
  __m128i tmp_var1= _mm_slli_si128(_mm_clmulepi64_si128(c,qz_up,1),8);
-                   //{buf1[0],0};
+                   //{0,buf1[0]};
 
  //long long int buf2[2]=_mm_clmulepi64_si128(c,qz_low,1);
  __m128i tmp_var2=_mm_srli_si128(_mm_clmulepi64_si128(c,qz_low,1),8);
-                   //{0,buf2[1]};
+                   //{buf2[1],0};
 
  __m128i c_mul_qz_1=tmp_var1+_mm_clmulepi64_si128(c,qz_up,0)+tmp_var2;
 
  __m128i tmp_var3=_mm_slli_si128(_mm_clmulepi64_si128(c,qz_low,1),8);
-                   //{buf2[0],0};
-
- __m128i c_mul_qz_0=tmp_var3+_mm_clmulepi64_si128(c,qz_low,0);
+                   //{0,buf2[0]};
+ long long *tv3_buf=&tmp_var3;
+ __m128i c_mul_qz_0=_mm_set_epi64x(tv3_buf[0],0)+_mm_clmulepi64_si128(c,qz_low,0);
 
 //ПРИВЕТ, КОСТЫЛЬ
- long long int* buf1=&c_mul_qz_1;
- long long int* buf0=&c_mul_qz_0;
+ unsigned long long int* buf1=&c_mul_qz_1;
+ unsigned long long int* buf0=&c_mul_qz_0;
  long long int buf[2];
 
  unsigned int d=deg(c_mul_qz_1);
@@ -120,47 +133,50 @@ __m128i second_mul_mod(const __m128i a, const __m128i b)
     }
 //ПОКА
 
- __m128i Mt={buf[1],buf[0]};
+ __m128i Mt={buf[0],buf[1]};
 
 //т.к. gz==qz_low, дальше не будем вводить новую переменную, а
 //будем использовать qz_low
 
 // long long int buf5[2]=_mm_clmulepi64_si128(Mt,qz_low,1);
  __m128i tmp_var4=_mm_slli_si128(_mm_clmulepi64_si128(Mt,qz_low,1),8);
-                   //{buf5[0],0};
+                   //{0,buf5[0]};
 
  c=tmp_var4+_mm_clmulepi64_si128(Mt,qz_low,0);
 
  return c+h;
  }
 
-int main(int argc, char *argv[])
+
+int main()
  {
- clock_t t1,t2;
- double dur;
- ll buf1[2],buf2[2];
+ for(;;)
+  {
+  clock_t t1,t2;
+  double dur;
+  long long int buf1[2],buf2[2];
 
- printf("Input your polynoms: ");
- scanf("%i %i",buf1+1,buf1);
- printf("a=(%i,%i)\nanother one:",buf1[1],buf1[0]);
- scanf("%i %i",buf2+1,buf2);
- printf("b=(%i,%i)\n\n\tFIRST\n",buf2[1],buf2[0]);
+  printf("Input your polynoms: ");
+  scanf("%lli %lli",&(buf1[1]),buf1);
+  printf("a=(%lli,%lli)\nanother one: ",buf1[1],buf1[0]);
+  scanf("%lli %lli",&(buf2[1]),buf2);
+  printf("b=(%i,%lli)\n\n\tFIRST\n",buf2[1],buf2[0]);
 
- __m128i a={buf1[1],buf2[0]},b={buf2[1],buf2[0]},c;
- t1=clock();
- c=first_mul_mod(a,b);
- t2=clock();
- dur=1000.0*(t2-t1)/CLOCKS_PER_SEC;
- print128_num(c);
- printf("CPU time used : %.2f ms\n\n\tSECOND\n", dur);
+          //big-endian!
+  __m128i a={buf1[0],buf1[1]},b={buf2[0],buf2[1]},c;
+  t1=clock();
+  c=first_mul_mod(a,b);
+  t2=clock();
+  dur=1000.0*(t2-t1)/CLOCKS_PER_SEC;
+  print128_num(c);
+  printf("CPU time used : %.2f ms\n\n\tSECOND\n", dur);
 
- t1=clock();
- c=second_mul_mod(a,b);
- t2=clock();
- dur=1000.0*(t2-t1)/CLOCKS_PER_SEC;
- print128_num(c);
- printf("CPU time used : %.2f ms\n\n", dur);
-
-
+  t1=clock();
+  c=second_mul_mod(a,b);
+  t2=clock();
+  dur=1000.0*(t2-t1)/CLOCKS_PER_SEC;
+  print128_num(c);
+  printf("CPU time used : %.2f ms\n\n", dur);
+  }
  return 0;
  }
--- mul_in_field.pro
+++ mul_in_field.pro
@@ -3,4 +3,7 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
+QMAKE_CFLAGS += -m64 -mpclmul
+
+
 SOURCES += main.c
